f0e07985dd894ad126e5d3a7e4a8f375a3c724aa
dohaelsawy <dohaelsawy18@gmail.com>
2024-09-15 12:45:43 +0300

    feat: basic implamentation to http datetime

--- cmd/net-http/main.go
+++ cmd/net-http/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	pkg "github.com/dohaelsawy/codescalers/datetimeserver/pkg/net-http"
+)
+
+
+func main() {
+
+	http.HandleFunc("/datetime", pkg.DateTime)
+
+	log.Println("Server started on port 8090")
+
+	http.ListenAndServe(":8090", nil)
+}
--- pkg/net-http/server.go
+++ pkg/net-http/server.go
@@ -1,1 +1,18 @@
 package pkg
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+
+
+func DateTime(w http.ResponseWriter , r *http.Request){
+
+	fmt.Fprintln(w,time.Now().Format("Jan 02, 2006"))
+	
+	fmt.Fprintln(w,time.Now().Format("3:04 PM"))
+	
+	w.WriteHeader(http.StatusOK)
+}

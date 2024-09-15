dac9692031ec596723b90eb6f9d12f58754780a0
dohaelsawy <dohaelsawy18@gmail.com>
2024-09-15 18:55:59 +0300

    feat: add tests for both servers

---
+++ test/gin/server_test.go
@@ -0,0 +1,60 @@
+package test
+
+import (
+	"net/http"
+	"net/http/httptest"
+
+	"testing"
+	"time"
+
+	pkg "github.com/dohaelsawy/codescalers/datetimeserver/pkg/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert/v2"
+)
+
+
+func SetUpRouter() *gin.Engine{
+    router := gin.Default()
+    return router
+}
+
+
+func TestServer(t *testing.T) {
+
+	testcase := []struct {
+		description string
+		status      int
+		path        string
+		method      string
+		expect      string
+	}{
+		{
+			description: "valid request",
+			status:      200 ,
+			path:        "/datetime",
+			method:      "GET",
+			expect:      time.Now().Format(time.UnixDate),
+		},
+		{
+			description: "invalid method request",
+			status:      404,
+			path:        "/datetime",
+			method:      "POST",
+			expect:      "404 page not found",
+		},
+	}
+
+	for _, test := range testcase {
+		t.Run(test.description, func(t *testing.T) {
+			r := SetUpRouter()
+			r.GET(test.path, pkg.DateTime)
+			req, _ := http.NewRequest(test.method, test.path, nil)
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+
+			assert.Equal(t,test.expect , w.Body.String())
+			assert.Equal(t, test.status, w.Code)
+		})
+	}
+
+}
---
+++ test/net-http/server_test.go
@@ -0,0 +1,54 @@
+package test
+
+import (
+	"net/http"
+	"net/http/httptest"
+
+	"testing"
+	"time"
+
+	pkg "github.com/dohaelsawy/codescalers/datetimeserver/pkg/net-http"
+	"github.com/go-playground/assert/v2"
+)
+
+func TestServer(t *testing.T) {
+
+	testcase := []struct {
+		description string
+		status      string
+		path        string
+		method      string
+		expect      string
+	}{
+		{
+			description: "valid request + matched time response",
+			status:      "200 OK",
+			path:        "/datetime",
+			method:      "GET",
+			expect:      time.Now().Format(time.UnixDate),
+		},
+		{
+			description: "invalid method request",
+			status:      "405 Method Not Allowed",
+			path:        "/datatime",
+			method:      "POST",
+			expect:      "",
+		},
+	}
+
+	for _, test := range testcase {
+		t.Run(test.description, func(t *testing.T) {
+			request, err := http.NewRequest(test.method, test.path, nil)
+			if err != nil {
+				t.Logf("%s", err.Error())
+			}
+			response := httptest.NewRecorder()
+
+			pkg.DateTime(response, request)
+
+			assert.Equal(t, test.expect, response.Body.String())
+			assert.Equal(t, test.status, response.Result().Status)
+		})
+	}
+
+}

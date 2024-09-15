627a34b4a07610bd8e7ad022961724582e421bd6
dohaelsawy <dohaelsawy18@gmail.com>
2024-09-15 12:56:48 +0300

    feat: basic implamentation to gin datetime

--- cmd/gin/main.go
+++ cmd/gin/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	pkg "github.com/dohaelsawy/codescalers/datetimeserver/pkg/gin"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	
+
+	r := gin.Default()
+
+	r.GET("/datetime",pkg.DateTime)
+
+	r.Run("localhost:8080")
+
 }
--- pkg/gin/server.go
+++ pkg/gin/server.go
@@ -1,3 +1,18 @@
 package pkg
 
+import (
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
+)
+
+
+func DateTime(c *gin.Context){
+	fmt.Fprintln(c.Writer,time.Now().Format("Jan 02, 2006"))
+	
+	fmt.Fprintln(c.Writer,time.Now().Format("3:04 PM"))
+	
+	c.Status(http.StatusOK)
+}
